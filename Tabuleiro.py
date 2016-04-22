7502089c733866f088270788b89a962bd617b28f
bxgomes <brunoxg@al.insper.edu.br>
2016-04-22 15:55:18 -0300

    Update Tabuleiro.py
    Não estou conseguindo enviar pelo Git Shell, então mandei direto pelo GitHub.
    
    Update: O tabuleiro, quando importar o jogo, vai conseguir mudar os botões para X e O;
    Criei uma função para cada botão, pois não sabia se dava nem como fazer uma função que funcionasse para todos;
    As funções ainda não estão completas

--- Tabuleiro.py
+++ Tabuleiro.py
@@ -1,21 +1,20 @@
 # -*- coding: utf-8 -*-
 import tkinter as tk
-from tkinter import *
 
 class Board:
     
     def __init__(self):
         self.window = tk.Tk()
-        self.window.geometry("300x350+100+100")
+        self.window.geometry("300x320+100+100")
         self.window.title("Jogo da Velha!")
         
-        self.window.rowconfigure(0, minsize=100)
-        self.window.rowconfigure(1, minsize=100)
-        self.window.rowconfigure(2, minsize=100)
-        self.window.rowconfigure(3, minsize=50)
-        self.window.columnconfigure(0, minsize=100)
-        self.window.columnconfigure(1, minsize=100)
-        self.window.columnconfigure(2, minsize=100)
+        self.window.rowconfigure(0, minsize = 100)
+        self.window.rowconfigure(1, minsize = 100)
+        self.window.rowconfigure(2, minsize = 100)
+        self.window.rowconfigure(3, minsize = 10)
+        self.window.columnconfigure(0, minsize = 100)
+        self.window.columnconfigure(1, minsize = 100)
+        self.window.columnconfigure(2, minsize = 100)
         
         self.button1 = tk.Button(self.window, height = 5, width = 10)
         self.button1.grid(row = 0, column = 0, sticky = "nsew")
@@ -45,22 +44,78 @@ class Board:
         self.button9.grid(row = 2, column = 2, sticky = "nsew")
         
         self.label_status = tk.Label()
-        self.label_status.configure(text = "Vez do jogador: X")
-        self.label_status.grid(row = 3, column = 0, columnspan = 3)
+        self.label_status.configure(text = "Próxima Jogada: X")
+        self.label_status.grid(row = 3, column = 0)
         
+    # fazer o button.configure(command) para os botões
+    
+    def button1_clicado(self):
         
-        menu = Menu(self.window)
-        self.window.config(menu=menu)
+        self.button1.configure(text = self.jogo.jogada, state = "disabled") # jogo.jogada vai ser imporado do Jogo()
+        # receber a jogada
+        self.label_status.configure("Vez do jogador: X") # mudar
+        # checar se acabou o jogo
         
-        file = Menu(menu)
-        file.add_command(label = "Exit", command = self. client_exit)
-        menu.add_cascade(label="File", menu = file)
+   
+    def button2_clicado(self):
         
+        self.button1.configure(text = self.jogo.jogada, state = "disabled") # jogo.jogada vai ser imporado do Jogo()
+        # receber a jogada
+        self.label_status.configure("Vez do jogador: X") # mudar
+        # checar se acabou o jogo
+     
+    def button3_clicado(self):
+        
+        self.button1.configure(text = self.jogo.jogada, state = "disabled") # jogo.jogada vai ser imporado do Jogo()
+        # receber a jogada
+        self.label_status.configure("Vez do jogador: X") # mudar
+        # checar se acabou o jogo
+     
+    def button4_clicado(self):
+        
+        self.button1.configure(text = self.jogo.jogada, state = "disabled") # jogo.jogada vai ser imporado do Jogo()
+        # receber a jogada
+        self.label_status.configure("Vez do jogador: X") # mudar
+        # checar se acabou o jogo
+     
+    def button5_clicado(self):
+        
+        self.button1.configure(text = self.jogo.jogada, state = "disabled") # jogo.jogada vai ser imporado do Jogo()
+        # receber a jogada
+        self.label_status.configure("Vez do jogador: X") # mudar
+        # checar se acabou o jogo
+     
+    def button6_clicado(self):
+        
+        self.button1.configure(text = self.jogo.jogada, state = "disabled") # jogo.jogada vai ser imporado do Jogo()
+        # receber a jogada
+        self.label_status.configure("Vez do jogador: X") # mudar
+        # checar se acabou o jogo
+        
+    def button7_clicado(self):
+        
+        self.button1.configure(text = self.jogo.jogada, state = "disabled") # jogo.jogada vai ser imporado do Jogo()
+        # receber a jogada
+        self.label_status.configure("Vez do jogador: X") # mudar
+        # checar se acabou o jogo
+        
+    def button8_clicado(self):
+        
+        self.button1.configure(text = self.jogo.jogada, state = "disabled") # jogo.jogada vai ser imporado do Jogo()
+        # receber a jogada
+        self.label_status.configure("Vez do jogador: X") # mudar
+        # checar se acabou o jogo
+        
+    def button9_clicado(self):
+        
+        self.button1.configure(text = self.jogo.jogada, state = "disabled") # jogo.jogada vai ser imporado do Jogo()
+        # receber a jogada
+        self.label_status.configure("Vez do jogador: X") # mudar
+        # checar se acabou o jogo
+    
     def iniciar(self):
         self.window.mainloop()
         
-    def client_exit(self):
-        exit()
         
 jdv = Board()
 jdv.iniciar()

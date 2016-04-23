fb2af4a87d1bd303640bf5fdd1a36bb1906c3620
bxgomes <brunoxg@al.insper.edu.br>
2016-04-23 18:49:17 -0300

    Update Jogo.py
    Ainda não consigo usar o GitShell
    
    Update:
    O jogo agora marca as jogadas feitas, quando ainda não há um ganhador
    
    Problemas:
    O jogo marca o jogador como X, O

--- Jogo.py
+++ Jogo.py
@@ -1,23 +1,36 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
-from numpy import *
 
 class Jogo:
     
     def __init__(self):
+        self.matriz = np.zeros([2, 2])
+        self.jogadores = ["X", "O"]
+        self.jogador = self.jogadores
                       
         
-        def recebe_jogada (self, i, j):
-            if self.contador % 2 == 0:
-                self.button9[" "] = "X"
-                self.matriz[2][2] = 1
-            else:
-                self.button9[" "] = "O"
-                self.matriz[i][j] = 2
-            
-            
-                
-                
+    def recebe_jogada (self, i, j):
         
+        ganhador = self.verifica_ganhador()
+        
+        if ganhador == -1:
+            if self.jogador == self.jogadores[0]:
+                self.matriz[i][j] = 1
+                self.jogador = self.jogadores[1]
+                ganhador = self.verifica_ganhador()
+                
+            elif self.jogador == self.jogadores[1]:
+                self.matriz[i][j] = 2
+                self.jogador = self.jogadores[0]
+                ganhador = self.verifica_ganhador()
             
+            
+                
+                
+    def verifica_ganhador(self):
+        # retorna -1 se ninguem ganhou
+        # retorna 0 se empatou
+        # retorna 1 se X ganhou
+        # retorna 2 se O ganhou
+        pass  

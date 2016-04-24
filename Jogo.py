d05cedcd131718611e5d2d69bed9d1e666cb8335
bxgomes <brunoxg@al.insper.edu.br>
2016-04-24 18:26:27 -0300

    Update Jogo.py
    Updates:
    Adição de espaçamentos e comentários para facilitar o entendimento

--- Jogo.py
+++ Jogo.py
@@ -1,39 +1,51 @@
 # -*- coding: utf-8 -*-
-
 import numpy as np
 
 class Jogo:
     
     def __init__(self):
-        self.matriz = np.zeros([3, 3])
+        self.matriz = np.zeros([3, 3]) # Na matriz: 0 = vazio, 1 = X e 2 = O
         self.contador = 0
-        #self.jogador
 
 
     def troca_jogador (self):
-        if self.verifica_ganhador() == -1:
-            if self.contador %2 == 0:
+        
+        if self.verifica_ganhador() == -1: # Caso ainda não tenha ganhador
+            
+            if self.contador % 2 == 0: # Quando o contador é um número par
+                
                 print ("contador =", self.contador)
                 print (" ")
-                return "Vez do jogador  X"
-            else:
+                return "Vez do jogador: X"
+            
+            else: # Quando o contador é impar
+            
                 print ("contador =", self.contador)
                 print (" ")
-                return "Vez do jogador  O"
-        elif self.verifica_ganhador() == 1:
+                return "Vez do jogador: O"
+                
+        elif self.verifica_ganhador() == 1: # Caso X tenha ganhado
+            
             return "Jogador X ganhou!"
-        elif self.verifica_ganhador() == 2:
+            
+        elif self.verifica_ganhador() == 2: # Caso O tenha ganhado
+            
             return "Jogador O ganhou!"
-        elif self.verifica_ganhador() == 0:
+            
+        elif self.verifica_ganhador() == 0: # Caso tenha dado empate
+    
             return "O jogo deu velha!"
-                
-                
+   
+   
     def limpa_jogadas (self):
-        self.matriz = np.zeros([3,3])
+    
+        self.matriz = np.zeros([3,3]) # Cria uma nova matriz
         print (self.matriz)
-                    
-                
+  
+   
     def verifica_ganhador(self):
+        
+        # retorna 0 em caso de empate, 1 caso X tenha ganhado, 2 caso O tenha ganhado e -1 caso contrário
         
         if self.matriz[0][0] == self.matriz[0][1] == self.matriz[0][2] == 1\
             or self.matriz[1][0] == self.matriz[1][1] == self.matriz[1][2] == 1\
@@ -43,6 +55,7 @@ class Jogo:
             or self.matriz[0][2] == self.matriz[1][2] == self.matriz[2][2] == 1\
             or self.matriz[0][0] == self.matriz[1][1] == self.matriz[2][2] == 1\
             or self.matriz[0][2] == self.matriz[1][1] == self.matriz[2][0] == 1:
+            # Essas são todas as possibilidades de vitória para o X
                 return 1
                 
         elif self.matriz[0][0] == self.matriz[0][1] == self.matriz[0][2] == 2\
@@ -53,6 +66,7 @@ class Jogo:
             or self.matriz[0][2] == self.matriz[1][2] == self.matriz[2][2] == 2\
             or self.matriz[0][0] == self.matriz[1][1] == self.matriz[2][2] == 2\
             or self.matriz[0][2] == self.matriz[1][1] == self.matriz[2][0] == 2:
+            # Essas são todas as possibilidades de vitória para o O   
                 return 2
                 
         elif self.matriz[0][0] == 0\
@@ -64,12 +78,15 @@ class Jogo:
             or self.matriz[2][0] == 0\
             or self.matriz[2][1] == 0\
             or self.matriz[2][2] == 0:
+            # Checar se ainda existe alguma casa vazia para poder continuar o jogo    
                 return -1
+        
         else:
+            # Em caso de empate
             return 0
-                 
         
     def recebe_jogada (self, i, j):
+        
         if self.verifica_ganhador() == 1\
             or self.verifica_ganhador == 2:
                 pass
@@ -92,5 +109,3 @@ class Jogo:
                 
         elif self.verifica_ganhador() == 0:
             pass
-
- 

7d496ed0bb9de96200408df74765499587cf6814
William Palmieri <williampr@al.insper.edu.br>
2016-04-25 08:29:20 -0300

    Removi as impressões

--- Jogo.py
+++ Jogo.py
@@ -13,15 +13,9 @@ class Jogo:
         if self.verifica_ganhador() == -1: # Caso ainda não tenha ganhador
             
             if self.contador % 2 == 0: # Quando o contador é um número par
-                
-                print ("contador =", self.contador)
-                print (" ")
                 return "Vez do jogador: X"
             
             else: # Quando o contador é impar
-            
-                print ("contador =", self.contador)
-                print (" ")
                 return "Vez do jogador: O"
                 
         elif self.verifica_ganhador() == 1: # Caso X tenha ganhado
@@ -40,7 +34,6 @@ class Jogo:
     def limpa_jogadas (self):
     
         self.matriz = np.zeros([3,3]) # Cria uma nova matriz
-        print (self.matriz)
   
    
     def verifica_ganhador(self):
@@ -97,14 +90,12 @@ class Jogo:
                 self.contador += 1
                 self.matriz[i][j] = 1
                 self.troca_jogador ()
-                print (self.matriz)
                 return "X"
                 
             else:
                 self.contador += 1
                 self.matriz[i][j] = 2
                 self.troca_jogador ()
-                print (self.matriz)
                 return "O"
                 
         elif self.verifica_ganhador() == 0:
